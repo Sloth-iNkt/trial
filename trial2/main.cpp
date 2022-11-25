5768211f6c4488fbcb1a59860290d1e31a8e7e1a
herseyy <herseyodasco1@gmail.com>
2022-11-26 03:06:41 +0800

    add value

--- trial2/main.cpp
+++ trial2/main.cpp
@@ -3,6 +3,9 @@
 #include <QDebug>
 #include <QtSql>
 
+void addValues(int id, QString name, QString hashed_pass);
+
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
@@ -12,19 +15,19 @@ int main(int argc, char *argv[])
     QSqlDatabase db;
     db = QSqlDatabase::addDatabase("QSQLITE");
 //    db.setDatabaseName("C:/Users/Hersey/school/pers/c++/trial/trial2/db.sqlite");
-    db.setDatabaseName("./db.sqlite");
+    db.setDatabaseName("./sqlite.db");
     if (!db.open()) {
         qDebug() << "Problem!";
     }
 
-    QString query = "CREATE TABLE users ("
+    QString query = "CREATE TABLE IF NOT EXISTS users ("
                     "ID integer primary key not null,"
                     "name VARCHAR(20) not null,"
                     "hashed_pass VARCHAR(20) not null);";
-    QString query1 = "CREATE TABLE category ("
+    QString query1 = "CREATE TABLE IF NOT EXISTS category ("
                     "ID integer PRIMARY KEY not null,"
                     "category VARCHAR(20) not null);";
-    QString query2 = "CREATE TABLE questionsEasy ("
+    QString query2 = "CREATE TABLE IF NOT EXISTS questionsEasy ("
                      "ID integer primary key not null,"
                      "question VARCHAR(100) not null,"
                      "answer BOOLEAN not null,"
@@ -32,7 +35,7 @@ int main(int argc, char *argv[])
                      "FOREIGN KEY(owner_id) REFERENCES users(ID),"
                      "category_id integer not null,"
                      "FOREIGN KEY(category_id) REFERENCES category(ID));";
-    QString query3 = "CREATE TABLE questionMedium ("
+    QString query3 = "CREATE TABLE IF NOT EXISTS questionMedium ("
                      "ID integer primary not null,"
                      "question VARCHAR(100) not null,"
                      "option1 VARCHAR(100) not null,"
@@ -44,7 +47,7 @@ int main(int argc, char *argv[])
                      "FOREIGN KEY(owner_id) REFERENCES users(ID),"
                      "category_id integer not null,"
                      "FOREIGN KEY(category_id) REFERENCES category(ID));";
-    QString query4 = "CREATE TABLE questionHard ("
+    QString query4 = "CREATE TABLE IF NOT EXISTS questionHard ("
                      "ID integer primary key not null,"
                      "question VARCHAR(100) not null,"
                      "answer VARCHAR(100) not null,"
@@ -52,7 +55,7 @@ int main(int argc, char *argv[])
                      "FOREIGN KEY(owner_id) REFERENCES users(ID),"
                      "category_id integer not null,"
                      "FOREIGN KEY(category_id) REFERENCES category(ID));";
-    QString query5 = "CREATE TABLE leaderboard ("
+    QString query5 = "CREATE TABLE IF NOT EXISTS leaderboard ("
                      "ID integer primary key not null,"
                      "name VARCHAR(20) not null,"
                      "score integer not null);";
@@ -67,9 +70,30 @@ int main(int argc, char *argv[])
         qDebug() << "Table is not created";
     }
 
+    addValues(1, "hi", "asdd");
+
+    db.close();
+
+
     qDebug() << "end";
 
     MainWindow w;
     w.show();
     return a.exec();
 }
+
+void addValues(int id, QString name, QString hashed_pass){
+    QSqlQuery qry;
+    qry.prepare("INSERT INTO users ("
+                "ID,"
+                "name,"
+                "hashed_pass)"
+                "VALUES (?, ?, ?);");
+    qry.addBindValue(id);
+    qry.addBindValue(name);
+    qry.addBindValue(hashed_pass);
+
+    if (!qry.exec()) {
+        qDebug() << "Error adding value to database";
+    }
+};
